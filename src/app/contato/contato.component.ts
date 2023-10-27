f0e94df44514e1fb50b508a751abde94f50d3f05
Jefferson Cardoso <maestro.jfc@gmail.com>
2023-10-27 16:07:47 -0300

    Implementação das validações do form Contato com o formReative.

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { NotificacoesService } from '../notificacoes.service';
 
 @Component({
   selector: 'app-contato',
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
-export class ContatoComponent {
+export class ContatoComponent implements OnInit {
+
+  formContato = this.formBuilder.group({
+    nome: ["", [Validators.minLength(4), Validators.required]],
+    assunto: ["", [Validators.minLength(4), Validators.required]],
+    telefone: ["", [Validators.minLength(11), Validators.required]],
+    email: ["", [Validators.email, Validators.required]],
+    mensagem: ["", [Validators.minLength(20), Validators.required]]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private msgEnviarForm: NotificacoesService
+  ){}
+
+  ngOnInit(): void {    
+  }
+
+  enviarFormulario(){
+    this.msgEnviarForm.notificar("Mensagem enviada com sucesso!");
+    this.formContato.reset();
+  }
 
 }
